82d5b7175777bffdda5268d6a83bdbbc7a1bee5e
Alvin <zyzzz0928@gmail.com>
2024-06-16 18:22:25 +0800

    map with resources

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,4 @@
-use crate::{player::Player, prelude::*, NUM_TILES};
+use crate::{prelude::*, NUM_TILES};
 
 #[derive(Clone, Copy, PartialEq)]
 pub enum TileType {
@@ -17,31 +17,47 @@ impl Map {
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm) {
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                match self.tiles[map_idx(x, y)] {
-                    TileType::Wall => {
-                        ctx.set(x, y, RGB::named(RED), RGB::named(BLACK), to_cp437('#'));
-                    }
-                    TileType::Floor => {
-                        ctx.set(x, y, RGB::named(GREEN), RGB::named(BLACK), to_cp437('.'));
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                if self.in_bounds(Point::new(x, y)) {
+                    let idx = map_idx(x, y);
+                    match self.tiles[idx] {
+                        TileType::Floor => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE,
+                                BLACK,
+                                to_cp437('.'),
+                            );
+                        }
+                        TileType::Wall => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE,
+                                BLACK,
+                                to_cp437('#'),
+                            );
+                        }
                     }
                 }
             }
         }
     }
 
-    pub fn in_bound(&self, p: Point) -> bool {
+    pub fn in_bounds(&self, p: Point) -> bool {
         p.x >= 0 && p.x < SCREEN_WIDTH && p.y >= 0 && p.y < SCREEN_HEIGHT
     }
 
     pub fn can_enter_tile(&self, p: Point) -> bool {
-        self.in_bound(p) && self.tiles[map_idx(p.x, p.y)] == TileType::Floor
+        self.in_bounds(p) && self.tiles[map_idx(p.x, p.y)] == TileType::Floor
     }
 
     pub fn try_idx(&self, p: Point) -> Option<usize> {
-        if !self.in_bound(p) {
+        if !self.in_bounds(p) {
             return None;
         }
         Some(map_idx(p.x, p.y))
@@ -50,29 +66,4 @@ impl Map {
 
 pub fn map_idx(x: i32, y: i32) -> usize {
     ((y * SCREEN_WIDTH) + x) as usize
-}
-
-pub struct State {
-    player: Player,
-    map: Map,
-}
-
-impl State {
-    pub fn new() -> Self {
-        let mut rng = RandomNumberGenerator::new();
-        let map_builder = MapBuilder::new(&mut rng);
-        Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-        }
-    }
-}
-
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
-    }
 }
