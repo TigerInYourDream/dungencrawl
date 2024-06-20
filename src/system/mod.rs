03c890cfb6959098f2b62cec2c95811b82a1ebc9
Alvin <zyzzz0928@gmail.com>
2024-06-20 23:00:23 +0800

    collisions

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,5 +1,6 @@
 use crate::prelude::*;
 
+mod collisions;
 mod entry_render;
 mod map_render;
 mod player_input;
@@ -7,6 +8,7 @@ mod player_input;
 pub fn build_schedule() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collision_system())
         .add_system(map_render::map_render_system())
         .add_system(entry_render::entity_render_system())
         .build()
